f42140b46a7c271627502384ecbd63a03b636413
UnTamedStArBlOb <qeu-b-458@zqueen.org>
2025-04-02 00:21:17 -0400

    Update Emitter.ts

--- src/Emitter.ts
+++ src/Emitter.ts
@@ -112,7 +112,7 @@ export class Emitter
         for (let i = 0; i < this.results.length; ++i)
         {
             out2 += printer.printNode(ts.EmitHint.Unspecified, this.results[i], resultFile);
-            out2 += '\n\n';
+            out2 += '\n\n newline \n';
             console.log(out2)
         }
 
